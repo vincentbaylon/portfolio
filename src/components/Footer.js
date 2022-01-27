ce416140bb70e9b7fa3ff720d56709a563a44772
VinceBaylon <vincentbaylon@gmail.com>
2022-01-27 02:41:44 -0800

    Footer style change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,25 @@ function Footer() {
 	}
 
 	return (
-		<div className='p-3 text-slate-800 flex items-center bg-gray-500'>
-			<h1 className='text-md md:text-lg flex-1'>
-				© 2022 Vincent Baylon -{' '}
-				<a
-					className='hover:underline hover:text-sky-500'
-					href={'mailto:' + 'vincentbaylon@gmail.com'}
-				>
-					vincentbaylon@gmail.com
-				</a>
-			</h1>
-			<div className=''>
-				<button
-					className='hover:underline hover:text-sky-500'
-					onClick={handleClick}
-				>
-					BACK TO TOP
-				</button>
+		<div className='p-3 text-slate-800 bg-gray-500 flex flex-row justify-center'>
+			<div className='md:max-w-6xl w-full flex flex-row items-center justify-center'>
+				<h1 className='text-md md:text-lg flex-1'>
+					© 2022 Vincent Baylon -{' '}
+					<a
+						className='hover:underline hover:text-sky-500'
+						href={'mailto:' + 'vincentbaylon@gmail.com'}
+					>
+						vincentbaylon@gmail.com
+					</a>
+				</h1>
+				<div className=''>
+					<button
+						className='hover:underline hover:text-sky-500'
+						onClick={handleClick}
+					>
+						BACK TO TOP
+					</button>
+				</div>
 			</div>
 		</div>
 	)
