543ee94e755b8561c6e7109f27a29854fd02cb3c
Vincent <vincentbaylon@gmail.com>
2022-10-24 19:47:15 -0700

    Style, skills update

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,16 @@
 function About() {
   return (
-    <div className='h-5/6 flex flex-col py-10 md:py-20'>
+    <div className="flex flex-col py-10 h-5/6 md:py-20">
       <h1
-        id='about'
-        className='p-2 my-5 w-auto text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white bg-gradient-to-r from-black font-light'
+        id="about"
+        className="w-auto p-2 my-5 text-3xl font-light text-white sm:text-4xl md:text-5xl lg:text-6xl bg-gradient-to-r from-black"
       >
         Get To Know Me
       </h1>
 
-      <div className='p-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 justify-center gap-6'>
-        <div className='text-lg 2xl:text-xl text-left'>
-          <h1 className='text-gray-600'>
+      <div className="grid justify-center grid-cols-1 gap-6 p-1 md:grid-cols-2 lg:grid-cols-2">
+        <div className="text-lg text-left 2xl:text-xl">
+          <h1 className="text-gray-600">
             I'm a full-stack developer currently located in Las Vegas. I'm
             passionate about being creative and developing web apps that
             positively impact people's lives. I'm focused and effective under
@@ -30,18 +30,19 @@ function About() {
           </h1>
         </div>
 
-        <div className=''>
-          <h1 className='text-2xl md:text-4xl font-bold text-gray-700'>
+        <div className="">
+          <h1 className="text-2xl font-bold text-gray-700 md:text-4xl">
             Skills
           </h1>
-          <h1 className='p-1 text-xl md:text-2xl text-gray-500'>
+          <h1 className="p-1 text-xl text-gray-500 md:text-2xl">
             React • JavaScript • HTML5 • CSS3 • BEM • SASS • TaildwindCSS • Ruby
-            on Rails • Ruby • PostgreSQL • SQL • Git • GitHub
+            on Rails • Ruby • PostgreSQL • SQL • Git • GitHub • React Native •
+            WordPress • Elementor
           </h1>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,13 @@
 function ProjectCard({ name, url, img, desc, tools, live, repo, children }) {
   return (
-    <div className="flex flex-col items-center text-center overflow-hidden">
-      <h1 className="p-2 text-2xl font-semibold underline text-black">
-        {name}
-      </h1>
+    <div className="flex flex-col items-center overflow-hidden text-center">
+      <h1 className="p-2 text-2xl font-semibold text-black">{name}</h1>
       {children}
-      <h1 className="p-2 text-gray-500 text-lg">{desc}</h1>
-      <h1 className="p-2 text-black text-lg">{tools}</h1>
+      <h1 className="p-2 text-lg text-gray-500">{desc}</h1>
+      <h1 className="p-2 text-lg text-black">{tools}</h1>
       <div className="flex flex-row gap-5">
         <a
-          className="text-gray-500 px-2 font-semibold hover:underline hover:decoration-rose-500 text-lg"
+          className="px-2 text-lg font-semibold text-gray-500 hover:underline hover:decoration-rose-500"
           href={url}
           target="_blank"
           rel="noreferrer noopener"
@@ -17,7 +15,7 @@ function ProjectCard({ name, url, img, desc, tools, live, repo, children }) {
           LIVE SITE
         </a>
         <a
-          className="text-gray-500 font-semibold hover:underline hover:decoration-rose-500 text-lg"
+          className="text-lg font-semibold text-gray-500 hover:underline hover:decoration-rose-500"
           href={repo}
           target="_blank"
           rel="noreferrer noopener"
