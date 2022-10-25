c409a39092142006f939444c5972401a27365094
Vincent <vincentbaylon@gmail.com>
2022-10-25 06:37:53 -0700

    Updated resume

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import resume from "./Resume.docx";
+import resume from "./Resume.pdf";
 import dev from "./assets/developer.svg";
 
 function Main({ handleProjectClick }) {
@@ -49,10 +49,10 @@ function Main({ handleProjectClick }) {
   });
 
   return (
-    <div className="flex flex-col md:flex-row items-center w-full justify-evenly">
-      <div className="mt-10 h-5/6 py-10 md:py-20 flex flex-col">
+    <div className="flex flex-col items-center w-full md:flex-row justify-evenly">
+      <div className="flex flex-col py-10 mt-10 h-5/6 md:py-20">
         <div className="">
-          <h1 className="font-bold text-3xl md:text-4xl lg:text-6xl text-black">
+          <h1 className="text-3xl font-bold text-black md:text-4xl lg:text-6xl">
             <div className="flex flex-row py-1">
               {displayStr}
               <motion.div
@@ -75,7 +75,7 @@ function Main({ handleProjectClick }) {
               </motion.div>
             </div>
           </h1>
-          <h1 className="font-bold text-3xl md:text-4xl lg:text-6xl text-black">
+          <h1 className="text-3xl font-bold text-black md:text-4xl lg:text-6xl">
             <div className="flex flex-row py-1">{displayStrTwo}</div>
           </h1>
         </div>
@@ -92,15 +92,15 @@ function Main({ handleProjectClick }) {
           <div className="flex flex-row gap-1">
             <span className="mx-1 mt-10">
               <button
-                className="mb-10 text-gray-500 border-2 border-gray-500 text-md rounded-sms py-2 px-4 hover:bg-gray-500 hover:text-white shadow-md"
+                className="px-4 py-2 mb-10 text-gray-500 border-2 border-gray-500 shadow-md text-md rounded-sms hover:bg-gray-500 hover:text-white"
                 onClick={handleProjectClick}
               >
                 PROJECTS
               </button>
             </span>
-            <span className="mt-10 mx-1">
+            <span className="mx-1 mt-10">
               <a href={resume} download>
-                <button className="mb-10 text-gray-500 border-2 border-gray-500 text-md rounded-sm py-2 px-4 hover:bg-gray-500 hover:text-white shadow-md">
+                <button className="px-4 py-2 mb-10 text-gray-500 border-2 border-gray-500 rounded-sm shadow-md text-md hover:bg-gray-500 hover:text-white">
                   RESUME
                 </button>
               </a>
